e84ed41da239d0b1dc30e1817a63d206faf8c63a
Jerónimo Luza <jero.luza@gmail.com>
2026-01-29 16:37:31 -0300

    added __init__.py to src/cpi

---
+++ src/cpi/__init__.py
@@ -0,0 +1,11 @@
+"""
+CPI (Consumer Price Index) analysis package.
+
+This package provides tools for:
+- Price scraping and data collection
+- COICOP classification
+- Price index calculation
+- CPI analysis and visualization
+"""
+
+__version__ = "0.1.0"
